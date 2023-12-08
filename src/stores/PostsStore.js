55e4aad01fba6f0b23b8e94e63cae52ebcaf0430
rosergeev <rosergeev@gmail.com>
2023-12-08 21:32:37 +0100

    Save new post to firestore in batch op

--- src/stores/PostsStore.js
+++ src/stores/PostsStore.js
@@ -2,7 +2,8 @@ import { defineStore } from 'pinia'
 import { useThreadsStore } from './ThreadsStore'
 import { useUsersStore } from './UsersStore'
 import { makeAppendChildToParent } from '@/helpers'
-import { fetchItem, fetchItems } from '../helpers'
+import { fetchItem, fetchItems, setItem } from '../helpers'
+import { getFirestore, doc, collection, arrayUnion, writeBatch } from 'firebase/firestore'
 
 export const usePostsStore = defineStore('PostsStore', {
   state: () => {
@@ -12,14 +13,30 @@ export const usePostsStore = defineStore('PostsStore', {
   },
   getters: {},
   actions: {
-    createPost(post) {
-      post.id = 'ggqq' + Math.random()
+    async createPost(post) {
       const userStore = useUsersStore()
       post.userId = userStore.authId
       post.publishedAt = Math.floor(Date.now() / 1000)
-      this.posts.push(post)
+      const db = getFirestore()
+      const batch = writeBatch(db)
+      const postRef = doc(collection(db, 'posts'))
+      const threadRef = doc(db, 'threads', post.threadId)
+      batch.set(postRef, post)
+      batch.update(threadRef, {
+        posts: arrayUnion(postRef.id),
+        contributors: arrayUnion(userStore.authId)
+      })
+      await batch.commit()
+      // const newPost = await addDoc(collection(db, 'posts'), post)
+      // await updateDoc(doc(db, 'threads', post.threadId), {
+      //   posts: arrayUnion(newPost.id),
+      //   contributors: arrayUnion(userStore.authId)
+      // })
+
+      setItem(this, 'posts', { ...post, id: postRef.id })
+
       const threadsStore = useThreadsStore()
-      this.appendPostToThread(threadsStore, { childId: post.id, parentId: post.threadId })
+      this.appendPostToThread(threadsStore, { childId: postRef.id, parentId: post.threadId })
       threadsStore.appendContributorToThread(threadsStore, {
         childId: userStore.authId,
         parentId: post.threadId
