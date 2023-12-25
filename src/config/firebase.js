20a87d61b44a8517e6224580e0c1bd77177c9ce2
rosergeev <rosergeev@gmail.com>
2023-12-25 22:01:45 +0100

    Use .env file for firebase configuration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyCiVUMhP92FvpRoyEdpr2jiygGwDp4LHfQ',
-  authDomain: 'vue-school-forum-6616e.firebaseapp.com',
-  projectId: 'vue-school-forum-6616e',
-  storageBucket: 'vue-school-forum-6616e.appspot.com',
-  messagingSenderId: '148563177248',
-  appId: '1:148563177248:web:f762385991eec353603145'
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
 
 export default firebaseConfig
